4c8195ef74873ea6783d1c23022181b712a2e702
tjulioh <thiagojulio20@gmail.com>
2020-10-22 16:34:02 -0300

    + Cadastro de Configuração de Evento + Cadastro de Motivo de Rescisao + Cadastro de Plano de Previdencia + Cadastro de Tipo de Afastamento + Cadastro de Tipo de Cargo
    Signed-off-by: Thiago Julio <thiagojulio20@gmail.com>

--- sistema_origem/ipm_cloud_postgresql/folha/rotinas_envio/pais.py
+++ sistema_origem/ipm_cloud_postgresql/folha/rotinas_envio/pais.py
@@ -4,31 +4,28 @@ import json
 import logging
 from datetime import datetime
 
+tipo_registro = 'pais'
+sistema = 300
+limite_lote = 500
+url = 'https://pessoal.cloud.betha.com.br/service-layer/v1/api/pais'
 
 def iniciar_processo_envio(params_exec, *args, **kwargs):
-    # Verifica se existe algum lote pendente de execução
-    # model.valida_lotes_enviados(params_exec)
-
-    # Realiza rotina de busca dos dados no cloud
     busca_dados(params_exec)
-
 
 def busca_dados(params_exec):
     print('- Iniciando busca de dados no cloud.')
-    url = 'https://pessoal.cloud.betha.com.br/service-layer/v1/api/pais'
     registros = interacao_cloud.busca_dados_cloud(params_exec, url=url)
     print(f'- Foram encontrados {len(registros)} registros cadastrados no cloud.')
     registros_formatados = []
-
     for item in registros:
-        hash_chaves = model.gerar_hash_chaves('300', 'paises', item['nome'])
+        hash_chaves = model.gerar_hash_chaves(sistema, tipo_registro, item['nome'])
         registros_formatados.append({
-        'sistema': 300,
-        'tipo_registro': 'paises',
-        'hash_chave_dsk': hash_chaves,
-        'descricao_tipo_registro': 'Cadastro de Paises',
-        'id_gerado': item['id'],
-        'i_chave_dsk1': item['nome']
+            'sistema': sistema,
+            'tipo_registro': tipo_registro,
+            'hash_chave_dsk': hash_chaves,
+            'descricao_tipo_registro': 'Cadastro de Pais',
+            'id_gerado': item['id'],
+            'i_chave_dsk1': item['nome']
         })
     model.insere_tabela_controle_migracao_registro2(params_exec, lista_req=registros_formatados)
     print('- Busca de paises finalizada. Tabelas de controles atualizas com sucesso.')
