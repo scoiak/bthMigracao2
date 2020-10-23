e1ac827c6d2dfe991eb41d4b4a36db4f4fa05011
Thiago Julio <thiagojulio20@gmail.com>
2020-10-23 09:49:10 -0300

    + Cadastro de CBO + Suporte para UTF-8 no retorno do SQL.
    Signed-off-by: Thiago Julio <thiagojulio20@gmail.com>

--- sistema_origem/ipm_cloud_postgresql/folha/sql_padrao/logradouro.sql
+++ sistema_origem/ipm_cloud_postgresql/folha/sql_padrao/logradouro.sql
@@ -7,9 +7,11 @@ select
 	-- public.bth_get_id_gerado('300', 'tipo-logradouro', (select substring(upper(tl.tplnome),1,20) from wun.tbtipolograd as tl where tl.tplcodigo = l.tplcodigo limit 1)) as tipoLogradouro,
 	-- public.bth_get_id_gerado('300', 'cidades', (select c.cidnome from wun.tbcidade as c where c.cidcodigo = l.cidcodigo limit 1)) as municipio,
 	(select id_gerado from public.controle_migracao_registro where hash_chave_dsk = md5(concat('300','tipo-logradouro', (select substring(upper(tl.tplnome),1,20) from wun.tbtipolograd as tl where tl.tplcodigo = l.tplcodigo limit 1))))as tipoLogradouro,
-	(select id_gerado from public.controle_migracao_registro where hash_chave_dsk = md5(concat('300','municipio', (select c.cidnome from wun.tbcidade as c where c.cidcodigo = l.cidcodigo limit 1)))) as municipio
+	(select id_gerado from public.controle_migracao_registro where hash_chave_dsk = md5(concat('300','cidades', (select c.cidnome from wun.tbcidade as c where c.cidcodigo = l.cidcodigo limit 1)))) as municipio,
+	public.bth_get_situacao_registro('300', 'logradouro', cast(logcodigo as varchar)) as situacaoRegistro
 from wun.tblogradouro as l
+limit 10
 ) as a
-where public.bth_get_situacao_registro('300', 'logradouro', upper(descricao),cast(municipio as varchar)) in (0) and
-municipio is not null
+where situacaoRegistro in (0)
+and municipio is not null
 and tipoLogradouro is not null
